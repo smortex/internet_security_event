c4ec00810d07811bb46946843b6cc3d2dc2b1099
Romain Tartière <romain@blogreen.org>
2019-02-27 06:30:12 -1000

    Make use of OpenSSL::SSL.verify_certificate_identity
    Instead of checking that a certificate subject or SAN match the
    hostname, rely on verify_certificate_identity which is part of Ruby's
    OpenSSL::SSL library.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Changed
+- Rely on `OpenSSL::SSL.verify_certificate_identity` to check that a certificate
+  is valid for the provided hostname.
+
 ## [1.1.0] - 2019-02-21
+
 ### Added
 - Add basic suport for TLSA events.
 
--- lib/internet_security_event/tls_status.rb
+++ lib/internet_security_event/tls_status.rb
@@ -33,36 +33,7 @@ module InternetSecurityEvent
     def hostname_is_valid_for_this_certificate?
       return true if hostname.nil?
 
-      hostname_match_subject? || hostname_match_subject_alternative_name?
-    end
-
-    def hostname_match_subject?
-      name_match_patern(hostname, common_name)
-    end
-
-    def hostname_match_subject_alternative_name?
-      return false unless certificate
-
-      san = certificate.extensions.select { |ext| ext.oid == 'subjectAltName' }.first
-
-      if san
-        alt_names = san.value.split(', ').map { |name| name.sub(/\ADNS:/, '') }
-        return true if alt_names.any? { |alt_name| name_match_patern(hostname, alt_name) }
-      end
-
-      false
-    end
-
-    def name_match_patern(hostname, pattern)
-      re = Regexp.new('\A' + pattern.split('*').map do |st|
-        Regexp.escape(st)
-      end.join('[^.]*') + '\z')
-
-      re.match(hostname)
-    end
-
-    def common_name
-      certificate.subject.to_a.select { |data| data[0] == 'CN' }.map { |data| data[1] }.first if certificate
+      OpenSSL::SSL.verify_certificate_identity(certificate, hostname)
     end
   end
 end
